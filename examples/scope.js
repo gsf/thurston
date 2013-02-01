5b6b53c10d15ada107d00f3e8d7ca1994d78182c
Gabriel Farrell <g@grrawr.com>
2013-02-01 16:25:25 -0500

    Split scope into js and th

--- examples/scope.js
+++ examples/scope.js
@@ -1,21 +1,3 @@
-/*
-// Thurston:
-
-blam: 'pink'
-
-jin: (){
-  blam: 'black'
-  bloop: 9
-  (){bloop: 12}() // IIFE
-  ret bloop
-}
-
-console.log(jin()) // 12
-console.log(blam) // 'black'
-*/
-
-// JavaScript:
-
 var blam = 'pink';
 
 var jin = function jin() {
